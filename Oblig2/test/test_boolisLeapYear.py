b4f179de73dd8f52e7cb008455350692d5c781b1
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2021-11-01 13:44:39 +0100

    commit, True set to False to check when expecting failure

--- Oblig2/test/test_boolisLeapYear.py
+++ Oblig2/test/test_boolisLeapYear.py
@@ -4,7 +4,7 @@ from boolisLeapYear import boolisLeapYear
 # A year is a leap year if it is divisible by 4 but not 100
 # underneath is a gathering of assertions for such years
 def test_year_is_leap_year_when_divisible_by_4_but_not_100():
-    assert boolisLeapYear(2024) is True
+    assert boolisLeapYear(2024) is False
     assert boolisLeapYear(1996) is True
 
     assert boolisLeapYear(1700) is False
@@ -13,7 +13,7 @@ def test_year_is_leap_year_when_divisible_by_4_but_not_100():
 
 # A year is not a leap year if it isn't divisible by 4
 def test_year_is_not_leap_year_when_not_divisible_by_4():
-    assert boolisLeapYear(2023) is True
+    assert boolisLeapYear(2023) is False
     assert boolisLeapYear(1937) is False
 
     assert boolisLeapYear(1932) is True
